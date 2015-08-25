a4a0ad44dd622cdc199a7b55210a450bfa65784e
Soren Smith <sorenrehkopfsmith@gmail.com>
2015-08-24 17:48:27 -0700

    index.js in models updated

--- models/index.js
+++ models/index.js
@@ -6,7 +6,11 @@ var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
+if(config.use_env_variable){
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+}else{
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+};
 var db        = {};
 
 fs

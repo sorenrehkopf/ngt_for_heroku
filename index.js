84d651bd60eab937e1f36d373002b3c0d08847de
Soren Smith <sorenrehkopfsmith@gmail.com>
2016-02-16 01:34:25 -0800

    touching up ux and code

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var db = require('./models')
+var db = require('./models');
 var express = require('express');
 var app = express();
+
+var qs = require('./public/assets/questionsAnswers.json');
+var resps = require('./public/assets/resps.json');
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -9,6 +12,14 @@ app.get('/',function(req, res){
 	db.user.findAll({order:[['score','ASC']]}).then(function(users){
 		res.render('index.ejs',{leaders:users});
 	})
+});
+
+app.get('/qs',function(req,res){
+	res.send(qs)
+});
+
+app.get('/resps',function(req,res){
+	res.send(resps)
 })
 
 app.get('/leaderboard',function(req, res){
